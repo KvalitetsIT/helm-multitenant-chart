74b6cb52b1c27589ee95bf6f7aa6e4b80267878b
Mikkel Birch <mib@kvalitetsit.dk>
2021-05-21 13:31:13 +0200

    build scripts

---
+++ build/push-repo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "hej"
+
+mkdir -p helm
+cd helm
+git config --global user.name "Github Actions"
+git config --global user.email "development@kvalitetsit.dk"
+git clone https://github.com/KvalitetsIT/helm-repo
+cd helm-repo
+cp ../../service-*.tgz service
+git add -A
+git commit -m "Adding new multitenant chart"
+git push https://$1@github.com/KvalitetsIT/helm-repo
