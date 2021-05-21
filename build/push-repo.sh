c50585726d2f3881399181dd9ab08201c3fc9262
Mikkel Birch <mib@kvalitetsit.dk>
2021-05-21 13:40:37 +0200

    multitenant insted of service in push-repo.sh

--- build/push-repo.sh
+++ build/push-repo.sh
@@ -8,7 +8,7 @@ git config --global user.name "Github Actions"
 git config --global user.email "development@kvalitetsit.dk"
 git clone https://github.com/KvalitetsIT/helm-repo
 cd helm-repo
-cp ../../service-*.tgz service
+cp ../../multitenant-*.tgz multitenant
 git add -A
 git commit -m "Adding new multitenant chart"
 git push https://$1@github.com/KvalitetsIT/helm-repo
