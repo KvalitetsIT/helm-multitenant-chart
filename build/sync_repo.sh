d720b0378d83fda7009bc53e0e4685e757d9b18a
Mikkel Birch <mib@kvalitetsit.dk>
2021-05-25 08:39:16 +0200

    generate indexfile03

--- build/sync_repo.sh
+++ build/sync_repo.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-mkdir -p /helm-repo/$chartName/
-mv $workspace/$chartName-* $workspace/helm-repo/$chartName/
 helm repo index . --url https://raw.githubusercontent.com/KvalitetsIT/helm-repo/master/
