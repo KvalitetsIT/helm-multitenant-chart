e43bff270d33e0a05770caaeaaef82959b8e3cb3
Mikkel Birch <mib@kvalitetsit.dk>
2021-05-25 08:50:36 +0200

    copy index to repo also

--- build/push-repo.sh
+++ build/push-repo.sh
@@ -8,7 +8,9 @@ git config --global user.name "Github Actions"
 git config --global user.email "development@kvalitetsit.dk"
 git clone https://github.com/KvalitetsIT/helm-repo
 cd helm-repo
+ls -a
 cp ../../multitenant-*.tgz multitenant
+cp ../../index.yaml .
 git add -A
 git commit -m "Adding new multitenant chart"
 git push https://$1@github.com/KvalitetsIT/helm-repo
